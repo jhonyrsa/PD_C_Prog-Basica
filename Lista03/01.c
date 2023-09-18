24a4a18598bcef60d6d87982baa9aea65cf7118b
Jhony Almeida <131838700+jhonyrsa@users.noreply.github.com>
2023-09-17 21:00:16 -0300

    Lista 03 part01 e outras listas a resolver

---
+++ Lista03/01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int i=0;
+    for(;i<=5; i++){
+        printf("%d x %d = %d\n", 3, i, 3*i);
+    }
+    return 0;
+}
---
+++ Lista03/02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int i;
+    for(i=1; i<=100; i++) printf("%d\n", i);
+    i = 1;
+    while(i<=100){
+        printf("%d\n", i);
+        i++;
+    }
+    i=1;
+    do{
+      printf("%d\n", i);
+      i++;
+    }while(i<=100);
+    return 0;
+}
